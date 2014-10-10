b83debc10cb722e2a74f080b0a9f6681c44d0bf5
David Farrell <davidnmfarrell@gmail.com>
2014-10-09 20:35:49 -0400

    Update architecture

--- lib/March.pm
+++ lib/March.pm
@@ -4,30 +4,36 @@ package March;
 
 use 5.020;
 use March::Log;
+use List::Util 'any';
 
-# ABSTRACT: A 2d web game engine, for turn based games written in Perl
+# ABSTRACT: A 2d logical game engine for turn based games written in Perl
+
 
 sub start ()
 {
     # configuration
-    March::Log->instance()->log_msg('Starting up');
+    my $log = March::Log->instance();
+	$log->msg_print('Starting up');
 
     # begin game loop
     while (1)
     {
-        # Process input
-        # Logical engine
-        # Render, SFX
-        last;
+        $log
+        last if $log->msg_exists('Game::End');
     }
-    March::Log->instance()->log_msg('Shutting down');
+	$log->msg_print('Game ending');
+	
+	$log->msg_print('Shutting down');
 }
 
 1;
 
 =head2 DESCRIPTION
 
-This is an experiment - a 2d web game engine, for turn based games written in Perl
+This is an experiment - a 2d logical game engine for turn based games written in Perl
 
 =cut
-
+__END__
+game_prep
+turns, phases, actor actions
+game_cleanup
--- lib/March/Log.pm
+++ lib/March/Log.pm
@@ -4,8 +4,10 @@ package March::Log;
 
 use feature qw/signatures say/;
 no warnings 'experimental';
+use List::Util 'any';
 
 my $instance = undef;
+my @msg_queue = ();
 
 sub instance
 {
@@ -13,7 +15,24 @@ sub instance
     return $instance;
 }
 
-sub log_msg ($self, $msg)
+sub msg_add ($self, $msg) { push @msg_queue, $msg }
+
+sub msg_delete ($self, $msg)
+{ 
+	for (0..$#msg_queue)
+    {
+        if ($msg_queue[$_] eq $msg)
+        {
+            splice @msg_queue, $_, 1;
+            last;
+        }
+    }
+}
+
+
+sub msg_exists ($self, $msg_string) { any { /^$msg_string$/ } @msg_queue }
+
+sub msg_print ($self, $msg)
 {
     my $FH = $self->{fh};
 
